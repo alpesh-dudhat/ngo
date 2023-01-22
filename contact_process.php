b63a6f62706d796e55abbd1042d4f2ea1f43d26c
Alpesh Dudhat <alpeshdudhat2010@gmail.com>
2023-01-22 20:22:07 +0530

    mail

--- contact_process.php
+++ contact_process.php
@@ -1,44 +1,76 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$to = "happy@yopmail.com";
-	$from = $_POST['email'];
-	$name = $_POST['name'];
-	$subject = $_POST['subject'];
-	$number = $_POST['number'];
-	$cmessage = $_POST['message'];
+if (isset($_POST['Email'])) {
 
-	$txt = "Name = " . $name . "\r\n  Email = " . $email . "\r\n Message =" . $cmessage;
+	// EDIT THE 2 LINES BELOW AS REQUIRED
+	$email_to = "happy@yopmail.com";
+	$email_subject = "New form submissions";
 
-	$headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: " . $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-	// $subject = "New Email For Foundation";
-
-	$logo = 'https://helpfriendfoundation.in/assets/img/logo/loader.png';
-	$link = 'https://helpfriendfoundation.in';
-
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
-
-	if ($email != NULL) {
-		mail($to, $subject, $txt, $headers);
+	function problem($error)
+	{
+		echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+		echo "These errors appear below.<br><br>";
+		echo $error . "<br><br>";
+		echo "Please go back and fix these errors.<br><br>";
+		die();
 	}
-	// $send = mail($to, $subject, $body, $headers);
-	// Redirect to a thank you page
-	header("Location: /index.html");
-	exit;
+
+	// validation expected data exists
+	if (
+		!isset($_POST['Name']) ||
+		!isset($_POST['Email']) ||
+		!isset($_POST['Message'])
+	) {
+		problem('We are sorry, but there appears to be a problem with the form you submitted.');
+	}
+
+	$name = $_POST['Name']; // required
+	$email = $_POST['Email']; // required
+	$message = $_POST['Message']; // required
+
+	$error_message = "";
+	$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+	if (!preg_match($email_exp, $email)) {
+		$error_message .= 'The Email address you entered does not appear to be valid.<br>';
+	}
+
+	$string_exp = "/^[A-Za-z .'-]+$/";
+
+	if (!preg_match($string_exp, $name)) {
+		$error_message .= 'The Name you entered does not appear to be valid.<br>';
+	}
+
+	if (strlen($message) < 2) {
+		$error_message .= 'The Message you entered do not appear to be valid.<br>';
+	}
+
+	if (strlen($error_message) > 0) {
+		problem($error_message);
+	}
+
+	$email_message = "Form details below.\n\n";
+
+	function clean_string($string)
+	{
+		$bad = array("content-type", "bcc:", "to:", "cc:", "href");
+		return str_replace($bad, "", $string);
+	}
+
+	$email_message .= "Name: " . clean_string($name) . "\n";
+	$email_message .= "Email: " . clean_string($email) . "\n";
+	$email_message .= "Message: " . clean_string($message) . "\n";
+
+	// create email headers
+	$headers = 'From: ' . $email . "\r\n" .
+		'Reply-To: ' . $email . "\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	@mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+	<!-- include your success message below -->
+
+	Thank you for contacting us. We will be in touch with you very soon.
+
+<?php
 }
+?>
