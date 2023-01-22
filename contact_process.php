216636205c56388a0f1198694260761dc8ec108f
Alpesh Dudhat <alpeshdudhat2010@gmail.com>
2023-01-22 19:41:41 +0530

    update home page and contact

--- contact_process.php
+++ contact_process.php
@@ -1,22 +1,22 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$to = "happy@yopmail.com";
+	$from = $_REQUEST['email'];
+	$name = $_REQUEST['name'];
+	$subject = $_REQUEST['subject'];
+	$number = $_REQUEST['number'];
+	$cmessage = $_REQUEST['message'];
 
-    $to = "alpeshdudhat2010@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
-
-    $headers = "From: $from";
+	$headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "Reply-To: " . $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+	// $subject = "New Email For Foundation";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+	$logo = 'https://helpfriendfoundation.in/assets/img/logo/loader.png';
+	$link = 'https://helpfriendfoundation.in';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
@@ -26,10 +26,15 @@
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
 	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
-    $send = mail($to, $subject, $body, $headers);
+	$send = mail($to, $subject, $body, $headers);
+	// Redirect to a thank you page
+	echo "Thank You! Your form has been successfully submitted.";
+	header("Location: /index.html");
+	exit;
+}
